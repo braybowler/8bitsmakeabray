40b65d41baeca18d2c0b5fcc4460b1bd84e5be88
Brayden Bowler <braybowler1995@gmail.com>
2024-11-07 14:50:46 -0500

    update sprites

--- src/components/WizardSprite.tsx
+++ src/components/WizardSprite.tsx
@@ -6,8 +6,8 @@ const WizardSprite = () => {
     return (
         <div className="flex justify-center align-middle ">
             <img
-                src={isDarkMode ? "../../public/images/necromancer.webp" : "../../public/images/wizard.webp"}
-                alt="8-bit red wizard"
+                src={ isDarkMode ? "../images/necromancer.webp" : "../images/wizard.webp" }
+                alt={ isDarkMode ? "8-bit necromancer" : "8-bit wizard" }
                 className="wizard-sprite-glow w-auto h-[300px] border-2 border-purple-400 dark:border-green-400 rounded-lg"
             />
         </div>
